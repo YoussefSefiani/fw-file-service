67b8136362b09e93bd6ece981168558e6e7f1fcf
YoussefSefiani <youssef.sefiani@student.ehb.be>
2022-06-04 21:07:56 +0200

    file image

--- src/main/java/fw/fileservice/controller/FileController.java
+++ src/main/java/fw/fileservice/controller/FileController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-@Controller
+@RestController
 @RequestMapping(path = "api")
 public class FileController {
 
